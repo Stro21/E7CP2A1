39f85bf156e5ee037516bbfd73da86cdd086e596
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-05 20:22:49 -0300

    Se avanza en el ejercicio 1.

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -2,12 +2,19 @@
 # rubocop:disable AsciiComments
 
 a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+print a
+puts "\n\n"
 
 # 1. Utilizando map generar un nuevo arreglo con cada valor aumentado en 1.
 
-
+b = a.map { |e| e + 1 }
+print b
+puts "\n\n"
 
 # 2. Utilizando map generar un nuevo arreglo que contenga todos los valores convertidos a float.
+
+
+
 # 3. Utilizando map generar un nuevo arreglo que contenga todos los valores convertidos a string.
 # 4. Utilizando reject descartar todos los elementos menores a 5 en el array.
 # 5. Utilizando select descartar todos los elementos mayores a 5 en el array.
