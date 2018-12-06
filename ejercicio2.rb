fab1e43cba08d5fc07bf3a9266642b96c3c594fe
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-05 21:26:39 -0300

    Se enpezó el ejercicio 2.

---
+++ ejercicio2.rb
@@ -0,0 +1,13 @@
+# rubocop:disable LineLength
+# rubocop:disable AsciiComments
+
+nombres = %w[Violeta Andino Clemente Javiera Paula Pia Ray]
+
+# 1. Extraer todos los elementos que excedan mas de 5 caracteres utilizando el método .select.
+# 2. Utilizando .map crear una arreglo con los nombres en minúscula.
+# 3. Utilizando .select para crear un arreglo con todos los nombres que empiecen con P.
+# 4. Utilizando .map crear un arreglo único con la cantidad de letras que tiene cada nombre.
+# 5. Utilizando .map y .gsub eliminar las vocales de todos los nombres.
+
+# rubocop:enable LineLength
+# rubocop:enable AsciiComments
