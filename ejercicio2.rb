46eeb93eb5e1abd4deeac42be86971c941209d76
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-05 21:34:36 -0300

    Se hizo la parte 2 del ejercicio 2.

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -4,7 +4,17 @@
 nombres = %w[Violeta Andino Clemente Javiera Paula Pia Ray]
 
 # 1. Extraer todos los elementos que excedan mas de 5 caracteres utilizando el método .select.
+
+a = nombres.select { |e| e.length < 5 }
+print a
+puts ''
+
 # 2. Utilizando .map crear una arreglo con los nombres en minúscula.
+
+b = nombres.map { |e| e.downcase! }
+print b
+puts ''
+
 # 3. Utilizando .select para crear un arreglo con todos los nombres que empiecen con P.
 # 4. Utilizando .map crear un arreglo único con la cantidad de letras que tiene cada nombre.
 # 5. Utilizando .map y .gsub eliminar las vocales de todos los nombres.
