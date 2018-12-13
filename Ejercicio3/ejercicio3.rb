6d361329a58087ff620504992c26fdccd2c97005
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-13 15:51:10 -0300

    Se creo un archivo para que esten todas las funciones del ejercicio 3.

--- Ejercicio3/ejercicio3.rb
+++ Ejercicio3/ejercicio3.rb
@@ -1,14 +1,6 @@
+require_relative 'ejercicio3_funciones.rb'
+
 alumnos = []
-
-def ingresar_persona(str1, str2, str3, str4)
-  persona = {}
-  persona[:nombre] = str1
-  persona[:edad] = str2
-  persona[:comuna] = str3
-  persona[:género] = str4
-  persona
-end
-
 alumnos.push(ingresar_persona('Eduardo', 29, 'Las Condes', 'Masculino'))
 print alumnos
 puts ''
---
+++ Ejercicio3/ejercicio3_funciones.rb
@@ -0,0 +1,8 @@
+def ingresar_persona(str1, str2, str3, str4)
+  persona = {}
+  persona[:nombre] = str1
+  persona[:edad] = str2
+  persona[:comuna] = str3
+  persona[:género] = str4
+  persona
+end
